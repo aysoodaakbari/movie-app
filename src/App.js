95a4f74aeec2b31ae1761adfdd1b1ba296b96d69
aysoodaakbari <aysoodaakbariii5515@gmail.com>
2022-06-11 19:03:00 +0430

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import MovieComponent from "./components/MovieComponent";
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Head =styled.div`
+width:100%;
+height:30px;
+background-color:#00ba25;
+`;
+const Header = styled.div`
+background-color:#919191;
+color: white;
+margin-left: auto;
+margin-right: auto;
+margin-top: 40px;
+align-items: center;
+padding: 10px;
+width:1000px;
 
+`;
+const SearchBox = styled.div`
+  display: flex;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 10px 10px;
+  border-radius: 6px;
+  width: 50%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;
+  `;
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+        <Head></Head>
+    <Header> <SearchBox>
+          <SearchIcon src=" " />
+          <SearchInput
+            placeholder="Search Movie"
+           
+          />
+        </SearchBox>
+    </Header>
+    <MovieListContainer></MovieListContainer>
+    </Container>
   );
 }
 
